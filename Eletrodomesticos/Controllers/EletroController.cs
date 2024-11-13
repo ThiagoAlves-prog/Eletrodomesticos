0cc91bd9419853a47a0215931b25e0f32155d339
ThiagoAlves-prog <thiago.j.alves7@aluno.senai.br>
2024-11-13 08:00:01 -0300

    Adicionar arquivos de projeto.

---
+++ Eletrodomesticos/Controllers/EletroController.cs
@@ -0,0 +1,20 @@
+﻿using Eletrodomesticos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eletrodomesticos.Controllers
+{
+    public class EletroController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Eletrodomestico> eletro = new List<Eletrodomestico>
+            {
+                new Geladeira {Marca = "Eletrolux", Modelo = "FrostFree Inverter", CapacidadeLitros = 450, ConsumoEnergetico = 47.9 , TemFreezer = true},
+                new Geladeira {Marca = "Continental", Modelo = "FrostFree", CapacidadeLitros = 450, ConsumoEnergetico = 47.9 , TemFreezer = false},
+                new Televisor {Marca = "Samsung", Modelo = "Crystal 4k", TamanhoTela = 55, ConsumoEnergetico = 110 , EhSmart = true}
+
+            };
+            return View(eletro);
+        }
+    }
+}
---
+++ Eletrodomesticos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Eletrodomesticos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
